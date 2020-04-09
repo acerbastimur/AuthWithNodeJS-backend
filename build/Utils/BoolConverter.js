3819574261f38903395d4430e90984efde0b36e2
acerbastimur <umutacerbastimur@gmail.com>
2020-04-09 18:15:59 +0300

    removed unnecessary folders

--- build/Utils/BoolConverter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var convertToBool = function (unparsedText) {
-    return unparsedText == "true" ? true : false;
-};
-exports.default = convertToBool;
